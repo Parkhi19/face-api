c33c1f4a702c6a6ac339f9321bfad94e427e91fc
Parkhi <parkhigoyal46@gmail.com>
2023-10-27 21:25:26 +0530

    corrected image names

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ Promise.all([
       labels.map(async label => {
         const descriptions = []
         for (let i = 1; i <= 2; i++) {
-          const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
+          const img = await faceapi.fetchImage(`https://github.com/Parkhi19/face-api/tree/main/labeled_images/${label}/${i}.jpg`)
           const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
           descriptions.push(detections.descriptor)
         }
